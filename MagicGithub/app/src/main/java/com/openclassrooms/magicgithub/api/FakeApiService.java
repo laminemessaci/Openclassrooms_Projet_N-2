38f875207af88ee1dd81ab5a31b7edf00bcfb0f7
messaci <lamine.messaci18@gmail.com>
2020-01-28 19:18:34 +0100

    TO DO done and Test OK
    FakeApiService & UserRepository

--- MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ MagicGithub/app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -14,8 +14,9 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
+
         // TODO: A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -25,6 +26,7 @@ public class FakeApiService implements ApiService {
     @Override
     public void generateRandomUser() {
         // TODO: A modifier
+        users.add(User.random());
     }
 
     /**
@@ -33,5 +35,6 @@ public class FakeApiService implements ApiService {
     @Override
     public void deleteUser(User user) {
         // TODO: A modifier
+        users.remove(user);
     }
 }
